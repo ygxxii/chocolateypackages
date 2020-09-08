9370e073ffd73a82291b81a8e22a082e06b9ab32
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-08 15:38:36 +0800

    Update icon and description, update chocolateyuninstall.ps1

--- automatic/qq/tools/chocolateyuninstall.ps1
+++ automatic/qq/tools/chocolateyuninstall.ps1
@@ -16,35 +16,32 @@ try {
 catch {
 }
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = '腾讯QQ'
-  fileType      = 'MSI'
-  silentArgs    = "/quiet /passive /qn /norestart"
-  validExitCodes= @(0, 3010, 1605, 1614, 1641)
-}
+# $packageArgs = @{
+#   packageName   = 'Tencent QQMail Plugin'
+#   softwareName  = 'Tencent QQMail Plugin'
+# }
 
-$uninstalled = $false
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+# $uninstalled = $false
+# [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
-if ($key.Count -eq 1) {
-  $key | % {
-    $packageArgs['file'] = "$($_.UninstallString)"
+# if ($key.Count -eq 1) {
+#   $key | % {
+#     $packageArgs['file'] = "$($_.UninstallString)"
 
-    if ($packageArgs['fileType'] -eq 'MSI') {
-      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+#     if ($packageArgs['fileType'] -eq 'MSI') {
+#       $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
 
-      $packageArgs['file'] = ''
-    } else {
-    }
+#       $packageArgs['file'] = ''
+#     } else {
+#     }
 
-    Uninstall-ChocolateyPackage @packageArgs
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Host "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Host "- $($_.DisplayName)"}
-}
+#     Uninstall-ChocolateyPackage @packageArgs
+#   }
+# } elseif ($key.Count -eq 0) {
+#   Write-Warning "$packageName has already been uninstalled by other means."
+# } elseif ($key.Count -gt 1) {
+#   Write-Warning "$($key.Count) matches found!"
+#   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+#   Write-Host "Please alert package maintainer the following keys were matched:"
+#   $key | % {Write-Host "- $($_.DisplayName)"}
+# }
