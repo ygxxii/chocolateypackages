be11a0cf3684b53b00b183f64f825fda6f09e78a
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-10 00:11:40 +0800

    Add new package dingtalk

---
+++ automatic/dingtalk/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url = 'https://www.dingtalk.com/win/d/'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'DingTalk*'
+  checksum       = '9d3122f4028f74f9c3ad85ef16a50baa65f131b03db8023cf6626e1fdffea92f'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/dingtalk/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'DingTalk'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType      = 'exe' #only one of these: MSI or EXE (ignore MSU for now)
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
