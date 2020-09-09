36b410e9e554a58500db7ef368421debea30671f
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-09 19:09:46 +0800

    Add new package: tencentdocs

---
+++ automatic/tencentdocs/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url        = 'https://down.qq.com/qqweb/PC_DOCS/TencentDocsSetup.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'TencentDocsSetup*'
+  checksum       = '4d735b30f76d84d455238b270d7b0a5aaa8d8d3e06c083f35268d5d4b9b56ee8'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
