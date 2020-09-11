cb3db72682fba9a977efe1ffd4cfd0b8347d8eff
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-12 01:33:35 +0800

    Create new package netease-cloud-music, testing

---
+++ automatic/netease-cloud-music/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$url        = 'https://d1.music.126.net/dmusic/cloudmusicsetup2.7.3.198319.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'cloudmusicsetup*'
+  checksum       = '8f352e1afb7fec2f988193e31aef0c93ff44750799d79baa362fb01fa58c95ee'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
