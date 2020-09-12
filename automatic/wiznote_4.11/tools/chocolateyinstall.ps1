9016cf0d47dd76a69df81110b1d58cc86782a355
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-12 21:34:59 +0800

    Create new package wiznote version 4.11

---
+++ automatic/wiznote_4.11/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$url        = 'http://get.wiz.cn/wiznote-windows-x86-2019-05-15.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName   = 'wiznote-windows*'
+  checksum      = 'ad0297ea90cfacfe62612df306963363eff3b6a229d791dbf1ccfd8bc42216ab'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/wiznote_4.11/tools/chocolateyuninstall.ps1
@@ -0,0 +1,74 @@
+﻿# WizNote set different "DisplayName" to:
+# HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Wiz_is1\DisplayName
+# base on system's language. Like:
+#   * zh_CN: "为知笔记*"
+#   * en_US: "WizNote*"
+# File an issue if you met different value.
+
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = "'WizNote ?.*'"
+  fileType       = 'exe'
+  silentArgs     = '/SILENT'
+  validExitCodes = @(0)
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+    else {
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % { Write-Warning "- $($_.DisplayName)" }
+}
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = "'为知笔记 ?.*'"
+  fileType       = 'exe'
+  silentArgs     = '/SILENT'
+  validExitCodes = @(0)
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+    else {
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % { Write-Warning "- $($_.DisplayName)" }
+}
