e6a6582f340225bcba94e4806cd52c976f21d3f3
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-12 21:33:12 +0800

    Update chocolateyuninstall.ps1 for wiznote_4.4, again

--- automatic/wiznote_4.4/tools/chocolateyuninstall.ps1
+++ automatic/wiznote_4.4/tools/chocolateyuninstall.ps1
@@ -1,18 +1,18 @@
 ﻿# WizNote set different "DisplayName" to:
 # HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Wiz_is1\DisplayName
 # base on system's language. Like:
-#   * zh_CN: "为知笔记 *"
-#   * en_US: "WizNote *"
+#   * zh_CN: "为知笔记*"
+#   * en_US: "WizNote*"
 # File an issue if you met different value.
 
 $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = "'WizNote ?.*'"
-  fileType      = 'exe'
-  silentArgs   = '/SILENT'
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = "'WizNote ?.*'"
+  fileType       = 'exe'
+  silentArgs     = '/SILENT'
+  validExitCodes = @(0)
 }
 
 $uninstalled = $false
@@ -24,17 +24,20 @@ if ($key.Count -eq 1) {
     if ($packageArgs['fileType'] -eq 'MSI') {
       $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
       $packageArgs['file'] = ''
-    } else {
+    }
+    else {
     }
     Uninstall-ChocolateyPackage @packageArgs
   }
-} elseif ($key.Count -eq 0) {
+}
+elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
+}
+elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | % { Write-Warning "- $($_.DisplayName)" }
 }
 
 $packageArgs = @{
