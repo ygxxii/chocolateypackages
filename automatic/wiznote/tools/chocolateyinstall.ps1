ac2e08d246fb8d9e700401b73aa29f3876408d24
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-12 21:47:48 +0800

    Create new package wiznote latest version

---
+++ automatic/wiznote/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$url        = 'http://url.wiz.cn/u/windows'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName   = 'wiznote-windows*'
+  checksum      = 'a1c4e1a35392985cf438c8ce5b7a8f7dc4f5f10c7912977a939b58e0dfaf741e'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
