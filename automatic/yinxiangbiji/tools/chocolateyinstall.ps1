7eab52f860c7fe729da08a1b1bc25b434ab55099
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-13 16:51:27 +0800

    Create new package yinxiangbiji

---
+++ automatic/yinxiangbiji/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url        = 'https://www.yinxiang.com/download/get.php?file=Win'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'Evernote_*'
+  checksum      = 'ae325f3c0ec66d4498c8a0a640050424064f66ffa02b3944bf690ff09ea7d17a'
+  checksumType  = 'sha256'
+  silentArgs    = "/quiet"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
