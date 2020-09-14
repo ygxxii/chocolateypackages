1128626adad05e6e8212bdd92b4b16aafe7566d1
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-14 09:12:48 +0800

    Create new package wecom

---
+++ automatic/wecom/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url        = 'https://work.weixin.qq.com/wework_admin/commdownload?platform=win&from=wwindex' # download url, HTTPS preferred
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'WXWork_*'
+  checksum      = '4aaff8d47c744c37329794ac21ee27439b8c0a0b7686372a313caec47fa41c4f'
+  checksumType  = 'sha256'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
