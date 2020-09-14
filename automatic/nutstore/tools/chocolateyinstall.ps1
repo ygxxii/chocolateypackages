f37554b291f9ff6e796ff8688c81c27b61cc40ce
whyso <16456434+qiexxii@users.noreply.github.com>
2020-09-14 09:11:12 +0800

    Create new package nutstore

---
+++ automatic/nutstore/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url = 'https://www.jianguoyun.com/static/exe/installer/NutstoreWindowsWPFInstaller.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = $url
+  softwareName   = 'NutstoreWindowsWPFInstaller*'
+  checksum       = '65e54f9b216334d364c06516ea5f3b778b251b677823147b38ed4f77ea7b27cf'
+  checksumType   = 'sha256'
+  silentArgs     = '/quiet'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/nutstore/tools/chocolateyuninstall.ps1
@@ -0,0 +1,78 @@
+﻿# Nutstore set different "DisplayName" to:
+# HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Wiz_is1\DisplayName
+# base on system's language. Like:
+#   * zh_CN: "坚果云"
+#   * en_US: "Nutstore"
+# File an issue if you met different value.
+
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = '坚果云'
+  fileType       = 'MSI'
+  silentArgs     = "/quiet /passive /qn /norestart"
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+$uninstalled = $false
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+    else {
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % { Write-Warning "- $($_.DisplayName)" }
+}
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'Nutstore'
+  fileType       = 'MSI'
+  silentArgs     = "/quiet /passive /qn /norestart"
+  validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
+
+$uninstalled = $false
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+    else {
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % { Write-Warning "- $($_.DisplayName)" }
+}
