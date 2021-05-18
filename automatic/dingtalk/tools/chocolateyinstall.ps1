f5c180b00545c302eed2077e4cc708c05ffa17d1
AnJia <anjia0532@gmail.com>
2021-05-18 16:00:52 +0800

    dingtalk-upgrade-to-6.0.15.5171263

--- automatic/dingtalk/tools/chocolateyinstall.ps1
+++ automatic/dingtalk/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url = 'https://www.dingtalk.com/win/d/'
+$url = 'https://dtapp-pub.dingtalk.com/dingtalk-desktop/win_installer/Release/DingTalk_v6.0.15.5171263.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
   url            = $url
   softwareName   = 'DingTalk_*'
-  checksum       = 'e1ce1b56fb5064b8586b076428fa4dabc16f6ac229c032eea82513ca85af30a4'
+  checksum       = '654F897D57064D588D8FE13060D1419F1B100E731D4CCD60A7774BEB8C1B8A73'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
